042074bba71f61d5ce5c9b108e0b0b5cdac5eb94
moseskhan2005-code <moseskhan2005@gmail.com>
2026-04-05 19:49:34 +0300

    Create layout.js
    Add root layout

---
+++ app/layout.js
@@ -0,0 +1,9 @@
+export default function RootLayout({ children }) {
+  return (
+    <html>
+      <body style={{ margin: 0, fontFamily: "sans-serif" }}>
+        {children}
+      </body>
+    </html>
+  );
+}
