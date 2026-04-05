f1cb4b054f865e2120e4a6086ab4defe4f456f2d
moseskhan2005-code <moseskhan2005@gmail.com>
2026-04-05 18:48:49 +0300

    Create page.js
    Add app directory

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ color: "white", background: "#121212", height: "100vh" }}>
+      <h1>THEPOUCH.GLOBAL</h1>
+      <p>Cinematic Finance OS is Live 🚀</p>
+    </main>
+  );
+  }
